16c903eb866b2a74de3c1e0c0f546ac516b72180
watorutart <watoru374215@gmail.com>
2026-06-05 22:49:30 +0900

    feat: add daily Haskell quiz interface and update corresponding study notes

--- study-memo/20260605/20260605.hs
+++ study-memo/20260605/20260605.hs
@@ -2,9 +2,21 @@ firstLetter :: String -> String
 firstLetter "" = "Empty string, whoops!"
 firstLetter all@(x:xs) = "The first letter of " ++ all ++ " is " ++ [x]
 
-bmiTell :: Double -> String
-bmiTell bmi
-    | bmi <= 18.5 = "You're underweight, you emo, you!"
-    | bmi <= 25.0 = "You're supposedly normal. Pffft, I bet you're ubly!"
-    | bmi <= 30.0 = "You're fat! Lose some weight, fatty!"
+bmiTell :: Double -> Double -> String
+bmiTell weight height
+    | weight / height ^ 2 <= 18.5 = "You're underweight, you emo, you!"
+    | weight / height ^ 2 <= 25.0 = "You're supposedly normal. Pffft, I bet you're ubly!"
+    | weight / height ^ 2 <= 30.0 = "You're fat! Lose some weight, fatty!"
     | otherwise = "You're a whale, congratulations!"
+
+max' :: (Ord a) => a -> a -> a
+
+max' a b
+    | a <= b = b
+    | otherwise = a
+
+myCompare :: (Ord a) => a -> a-> Ordering
+a `myCompare` b
+    | a == b = EQ
+    | a <= b = LT
+    | otherwise = GT
