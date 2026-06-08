2c932f463e26241ad8127ae24c98594543ec4c69
watorutart <watoru374215@gmail.com>
2026-06-08 22:28:44 +0900

    feat: add Haskell study notes and BMI calculation logic for June 8, 2026

---
+++ study-memo/20260608/20260608.hs
@@ -0,0 +1,21 @@
+bmiTell :: Double -> Double -> String
+bmiTell weight height
+    | bmi <= skinny = "You're underweight, you emo, you!"
+    | bmi <= normal = "You're supposedly normal. \
+        \Pffft, I bet you're ugly!"
+    | bmi <= fat = "You're fat! Lose some weight, fatty!"
+    | otherwise = "You're a whale congratulations!"
+    where bmi = weight / height ^ 2
+          skinny = 18.5
+          normal = 25.0
+          fat = 30.0
+
+
+badGreeting :: String
+badGreeting = "Oh! Pfft. It's you."
+niceGreeting :: String
+niceGreeting = "Hello! So very nice to see you,"
+greet :: String -> String
+greet "Juan" = niceGreeting ++ " Juan!"
+greet "Fernando" = niceGreeting ++ " Fernando!"
+greet name = badGreeting ++ " " ++ name
